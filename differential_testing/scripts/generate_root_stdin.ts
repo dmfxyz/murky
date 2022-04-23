0f688d15cb3dcba25a5f77dfbae7e05ab6c939cc
dmfxyz <d@dmf.xyz>
2022-04-23 15:57:34 -0700

    differential fuzzing w/ typescript working

--- differential_testing/scripts/generate_root_stdin.ts
+++ differential_testing/scripts/generate_root_stdin.ts
@@ -3,7 +3,9 @@ import { ethers } from 'ethers';
 import { toBuffer } from 'ethereumjs-util';
 
 const encoder = ethers.utils.defaultAbiCoder;
-const decoded_data = encoder.decode(["bytes32[4]"], process.argv[2])[0]
+const num_leafs = process.argv[2];
+const encoded_leafs = process.argv[3];
+const decoded_data = encoder.decode([`bytes32[${num_leafs}]`], encoded_leafs)[0]
 var dataAsBuffer = decoded_data.map(b => toBuffer(b));
 
 const tree = new MerkleTree(dataAsBuffer);
